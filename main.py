1727508de2f42036a1310fe1a75d463336e6acd9
EC2 Default User <ec2-user@ip-172-31-87-129.ec2.internal>
2019-11-03 05:04:20 +0000

    All the forms into one page.

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import logging
 import time
 from google.appengine.ext import ndb
 from models import Tree_Application
-from webapp2_extras import sessions
-
+​
+​
 the_jinja_env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
@@ -16,7 +16,7 @@ class CoordsRequest(ndb.Model):
     lat = ndb.StringProperty(required = True)
     lon = ndb.StringProperty(required = True)
     timestamp = ndb.DateTimeProperty(auto_now_add = True)
-
+​
 class AddressRequest(ndb.Model):
     address = ndb.StringProperty(required = True)
     timestamp = ndb.DateTimeProperty(auto_now_add = True)
@@ -24,76 +24,95 @@ class AddressRequest(ndb.Model):
 def run_query(location, o_name, a_name, o_email, a_email, o_phone_num, a_phone_num, tree_number, tree_species, date):
     user_info = Tree_Application(address = location, owner_name = o_name, applicant_name = a_name, owner_email = o_email, applicant_email = a_email, owner_phone_num = o_phone_num, applicant_phone_num = a_phone_num, num_of_trees = tree_number, tree_type = tree_species, date_submitted = date)
     info_key = user_info.put()
-
-class BaseHandler(webapp2.RequestHandler):
-    def dispatch(self):
-        # Get a session store for this request.
-        self.session_store = sessions.get_store(request=self.request)
-
-        try:
-            # Dispatch the request.
-            webapp2.RequestHandler.dispatch(self)
-        finally:
-            # Save all sessions.
-            self.session_store.save_sessions(self.response)
-
-    @webapp2.cached_property
-    def session(self):
-        # Returns a session using the default cookie key.
-        return self.session_store.get_session()    
-
-class WelcomePageHandler(BaseHandler):
+​
+# class webapp2.RequestHandler(webapp2.webapp2.RequestHandler):
+#     def dispatch(self):
+#         # Get a request store for this request.
+#         self.request_store = requests.get_store(request=self.request)
+#         print("request loading %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+​
+#         try:
+#             # Dispatch the request.
+#             webapp2.webapp2.RequestHandler.dispatch(self)
+#         finally:
+#             # Save all requests.
+#             print("saving request $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
+#             self.request_store.save_requests(self.response)
+​
+#     @webapp2.cached_property
+#     def request(self):
+#         # Returns a request using the default cookie key.
+#         return self.request_store.get_request()    
+​
+class WelcomePageHandler(webapp2.RequestHandler):
     def get(self):
         welcome_template = the_jinja_env.get_template('design_pages/welcome.html')
         self.response.write(welcome_template.render())
         
-class PersonalContactHandler(BaseHandler):
+class FormHandler(webapp2.RequestHandler):
     def get(self):
         personal_contact_template = the_jinja_env.get_template('form_pages/personal_contact.html')
         self.response.write(personal_contact_template.render())
-
-class SiteInfoHandler(BaseHandler):
-    def get(self):
-
-        self.session['ownname'] = self.request.get('ownname')
-        self.session['appliname'] = self.request.get('appliname')
-        self.session['ownemail'] = self.request.get('ownemail')
-        self.session['appliemail'] = self.request.get('appliemail')
-        self.session['ownphone'] = self.request.get('ownphone')
-        self.session['appliphone'] = self.request.get('appliphone')
-        self.session['submission'] = self.request.get('submission')
+​
+# class SiteInfoHandler(webapp2.RequestHandler):
+#     def get(self):
+​
+# <<<<<<< HEAD
+​
+#         self.request['ownname'] = self.request.get('ownname')
+#         self.request['appliname'] = self.request.get('appliname')
+#         self.request['ownemail'] = self.request.get('ownemail')
+#         self.request['appliemail'] = self.request.get('appliemail')
+#         self.request['ownphone'] = self.request.get('ownphone')
+#         self.request['appliphone'] = self.request.get('appliphone')
+#         self.request['submission'] = self.request.get('submission')
+#         self.request['address'] = self.request.get('address')
+   
+#         site_info_template = the_jinja_env.get_template('form_pages/site_info.html')
+#         self.response.write(site_info_template.render())
+# =======
+#         self.session['ownname'] = self.request.get('ownname')
+#         self.session['appliname'] = self.request.get('appliname')
+#         self.session['ownemail'] = self.request.get('ownemail')
+#         self.session['appliemail'] = self.request.get('appliemail')
+#         self.session['ownphone'] = self.request.get('ownphone')
+#         self.session['appliphone'] = self.request.get('appliphone')
+#         self.session['submission'] = self.request.get('submission')
         
-        myDict = {
-            'address_from_python_dict': '277 River Road'
-        }
-
-        site_info_template = the_jinja_env.get_template('form_pages/site_info.html')
-        self.response.write(site_info_template.render(myDict))
+#         myDict = {
+#             'address_from_python_dict': '277 River Road'
+#         }
+​
+#         site_info_template = the_jinja_env.get_template('form_pages/site_info.html')
+#         self.response.write(site_info_template.render(myDict))
+# >>>>>>> b9ec29afe5554df3029e3e9de5498755f6e01ddb
         
-class TermsAndConditionsHandler(BaseHandler):
-    def get(self):
+# class TermsAndConditionsHandler(webapp2.RequestHandler):
+#     def get(self):
         
-        self.session['tree'] = self.request.get('tree')
-        self.session['quantity'] = self.request.get('quantity')
+#         self.request['tree'] = self.request.get('tree')
+#         self.request['quantity'] = self.request.get('quantity')
+#         print("in terms and conditions handler: ", self.request.get('ownemail'))
        
-        terms_conditions_template = the_jinja_env.get_template('form_pages/terms_conditions.html')
-        self.response.write(terms_conditions_template.render())
+#         terms_conditions_template = the_jinja_env.get_template('form_pages/terms_conditions.html')
+#         self.response.write(terms_conditions_template.render())
         
-class ViewApplicationHandler(BaseHandler):
+class ViewApplicationHandler(webapp2.RequestHandler):
     def post(self):
         
         view_application_template = the_jinja_env.get_template('background_database/view_application.html')
         
-        address = self.session.get('address')
-        owner_name = self.session.get('ownname')
-        applicant_name = self.session.get('appliname')
-        owner_email = self.session.get('ownemail')
-        applicant_email = self.session.get('appliemail')
-        owner_phone_num = self.session.get('ownphone')
-        applicant_phone_num = self.session.get('appliphone')
-        num_of_trees = self.session.get('quantity')
-        tree_type = self.session.get('tree')
-        date_submitted = self.session.get('submission')
+        address = self.request.get('address')
+        owner_name = self.request.get('ownname')
+        applicant_name = self.request.get('appliname')
+        owner_email = self.request.get('ownemail')
+        applicant_email = self.request.get('appliemail')
+        owner_phone_num = self.request.get('ownphone')
+        applicant_phone_num = self.request.get('appliphone')
+        num_of_trees = self.request.get('quantity')
+        tree_type = self.request.get('tree')
+        date_submitted = self.request.get('submission')
+        
      
         run_query(address, owner_name, applicant_name, owner_email, applicant_email, owner_phone_num, applicant_phone_num, num_of_trees, tree_type, date_submitted)
         
@@ -113,12 +132,12 @@ class ViewApplicationHandler(BaseHandler):
         self.response.write(view_application_template.render(the_variable_dict))
         
         
-class SubmitPageHandler(BaseHandler):
+class SubmitPageHandler(webapp2.RequestHandler):
     def get(self):
-        submit_template = the_jinja_env.get_template('welcome_submit_pages/submit.html')
+        submit_template = the_jinja_env.get_template('design_pages/submit.html')
         self.response.write(submit_template.render())
         
-class ViewableDatabase(BaseHandler):
+class ViewableDatabase(webapp2.RequestHandler):
     def get(self):
         all_data_template = the_jinja_env.get_template('background_database/viewable_database.html')
      
@@ -129,19 +148,13 @@ class ViewableDatabase(BaseHandler):
         }
         
         self.response.write(all_data_template.render(the_variable_dict))
-
-config = {}
-config['webapp2_extras.sessions'] = {
-    'secret_key': 'my-super-secret-key',
-}
-
+​
+​
 app = webapp2.WSGIApplication([
     ('/', WelcomePageHandler),
-    ('/form1', PersonalContactHandler),
-    ('/form2', SiteInfoHandler),
-    ('/form3', TermsAndConditionsHandler),
+    ('/form', FormHandler),
     ('/result', ViewApplicationHandler),
     ('/complete', SubmitPageHandler),
     ('/database', ViewableDatabase),
-
-], debug=True, config=config)
+​
+], debug=True)
